88807b2ef26318b214d5636f390add00e4573e2d
Akanksha Chaudhari <aschaudhari@wisc.edu>
2025-03-13 17:22:31 -0500

    hwacc: Fix off-by-one error in RegisterBank address range
    Corrects the address range construction to properly represent
    [addr, addr + size), ensuring isSubset() checks function as expected.

--- benchmarks/sys_validation/bfs/bfs_clstr_hw_defines.h
+++ benchmarks/sys_validation/bfs/bfs_clstr_hw_defines.h
@@ -1,16 +1,16 @@
 //BEGIN GENERATED CODE
 //Cluster: BFS_CLSTR
 //NonCoherentDMA
-#define DMA_Flags 0x10020000
-#define DMA_RdAddr 0x10020001
-#define DMA_WrAddr 0x10020009
-#define DMA_CopyLen 0x10020011
+#define DMA_Flags 0x2f000000
+#define DMA_RdAddr 0x2f000001
+#define DMA_WrAddr 0x2f000009
+#define DMA_CopyLen 0x2f000011
 //Accelerator: TOP
-#define TOP 0x10020040
+#define TOP 0x2f000040
 //Accelerator: BFS
-#define BFS 0x10020080
-#define NODES 0x100200c0
-#define EDGES 0x10020900
-#define LEVELS 0x10024940
-#define LEVELCOUNTS 0x10024a80
+#define BFS 0x2f000080
+#define NODES 0x2f0000c0
+#define EDGES 0x2f000900
+#define LEVELS 0x2f004940
+#define LEVELCOUNTS 0x2f004a80
 //END GENERATED CODE
