e6974ccae71427677712672df28ffb41902e7484
Akanksha Chaudhari <aschaudhari@wisc.edu>
2025-05-16 12:51:29 -0500

    misc: Moving out gem5-SALAM's benchmark suite

--- benchmarks/sys_validation/mergesort/defines.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DEFINES_H__
-#define __DEFINES_H__
-
-#define CHECK
-
-#define TYPE uint32_t
-#define SIZE 2048
-
-#endif
--- benchmarks/test-cases/geptest/sw/bench.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEFINES
-#include "../defines.h"
-#endif
-
-volatile int stage;
-
-#include "../geptest_clstr_hw_defines.h"
